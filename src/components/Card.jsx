373270c410be9fe5dea0f4b7b1efc49d551c54b2
manuelurruti <manuelurrutimarquez@gmail.com>
2023-02-25 19:53:11 -0300

    cambie descripcion a espaniol

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,16 +6,20 @@ import TextField from '@mui/material/TextField';
 function Card() {
     const apiKey = "134d26ac91119b88ca2ffc3d8cb365ea";
     const [temperatura, setTemperatura] = useState("");
+    const [sensTermica, setSensTermica] = useState("");
     const [descripcion, setDescripcion] = useState("");
     const [icono, setIcono] = useState("");
     const [city, setCity] = useState("");
     const [loading, setLoading] = useState(true)
+
+    const [humidity, setHumidity] = useState("")
+
     const [url, setUrl] = useState(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+        `https://api.openweathermap.org/data/2.5/weather?appid=&lang=es&q=${city}&units=metric&appid=${apiKey}`
     );
     const handleFilterBySearch = (search) => {
         setUrl(
-            `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${apiKey}`
+            `https://api.openweathermap.org/data/2.5/weather??appid=&lang=es&q=${search}&units=metric&appid=${apiKey}`
         );
 
         setCity(search);
@@ -28,9 +32,13 @@ function Card() {
             .then((data) => {
                 if (data.main) {
                     console.log(data);
-                    setTemperatura(data.main.temp);
+                    setSensTermica(data.main.feels_like);
                     setDescripcion(data.weather[0].description);
                     setCity(data.name);
+
+                    setHumidity(data.main.humidity)
+
+                    setTemperatura(data.main.temp)
 
                     setIcono(
                         `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
@@ -44,7 +52,7 @@ function Card() {
     return (
         <div className="container">
             <Switchx></Switchx>
-            {temperatura <= 10 ? <img src="https://noticiasdeescobar.com/wp-content/uploads/2019/08/invierno-gente-abrigada.jpg" className="image"></img> : <img src="https://estaticos.elperiodico.com/resources/jpg/2/6/messi-saluda-grada-animacion-tras-anotar-durante-partido-liga-entre-barcelona-eibar-1547412391062.jpg" className="image"></img>}
+            {sensTermica <= 10 ? <img src="https://noticiasdeescobar.com/wp-content/uploads/2019/08/invierno-gente-abrigada.jpg" className="image"></img> : <img src="https://estaticos.elperiodico.com/resources/jpg/2/6/messi-saluda-grada-animacion-tras-anotar-durante-partido-liga-entre-barcelona-eibar-1547412391062.jpg" className="image"></img>}
 
 
             <form method="GET" className="centering">
@@ -54,9 +62,14 @@ function Card() {
                 </TextField>
             </form>
             {city.length == 0 ? <h2>busca tu Ciudad!</h2> : (<>  <h2>Ciudad de: {city}</h2>
-                <h3>Temperatura: {temperatura}</h3>
+                <h2>Temperatura: {temperatura}</h2>
+                <h3>Sensacion termica: {sensTermica}</h3>
                 <p>Descripcion: {descripcion} </p>
+
+                <p>Humedad: {humidity}</p>
+
                 <img src={icono} className="ChangeColor"></img></>)}
+            <h2>Te recomendamos vestirte con: </h2>
             <div>
             </div>
 
